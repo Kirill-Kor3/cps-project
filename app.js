633b74dd6cdfa0bbcb1e358b4955f4339c5975d6
Kirill-Kor3 <kzwork3@gmail.com>
2024-07-02 23:10:27 +0500

    made a slider

---
+++ app.js
@@ -0,0 +1,68 @@
+const swiper = new Swiper('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+      spaceBetween: 30,
+      centeredSlides: true,
+
+    breakpoints: { 
+        320: { 
+            enabled: true, 
+        }, 
+        768: { 
+            enabled: false, 
+        } 
+    },
+});
+
+const showMore = document.querySelector('.brands__more');
+const slides = document.querySelectorAll('.brands__slide');
+const totalSlides = slides.length;
+let items = getInitialItems();
+let isExpanded = false;
+
+function getInitialItems() {
+    return window.innerWidth > 992 ? 8 : 6;
+}
+
+function updateVisibleItems() {
+    const array = Array.from(document.querySelector('.brands__slider-wrapper').children);
+    const visItems = array.slice(0, items);
+    const hidItems = array.slice(items);
+
+    visItems.forEach(el => {
+        el.classList.add('brands__visible');
+    });
+    hidItems.forEach(el => {
+        el.classList.remove('brands__visible');
+    });
+}
+
+showMore.addEventListener('click', () => {
+    if (!isExpanded) {
+        // Показать больше карточек
+        items = Math.min(items + 5, totalSlides);
+        showMore.textContent = 'Скрыть';
+        showMore.style.background = '#F8F8F8 url(../img/icon.svg) no-repeat left center';
+    } else {
+        // Скрыть карточки
+        items = getInitialItems();
+        showMore.textContent = 'Показать все';
+        showMore.style.background = '#F8F8F8 url(../img/expand.svg) no-repeat left center';
+    }
+    
+    updateVisibleItems();
+    isExpanded = !isExpanded;
+});
+
+// Обновить видимость карточек при изменении размера окна
+window.addEventListener('resize', () => {
+    if (!isExpanded) {
+        items = getInitialItems();
+        updateVisibleItems();
+    }
+});
+
+// Инициализация видимости карточек при загрузке страницы
+updateVisibleItems();
