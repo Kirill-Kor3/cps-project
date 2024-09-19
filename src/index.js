fbee4968c6b602a1534e42726e142f75ef295ab6
Kirill-Kor3 <kzwork3@gmail.com>
2024-09-19 18:38:18 +0500

    finished the layout

--- src/index.js
+++ src/index.js
@@ -28,14 +28,29 @@ const swiper = new Swiper('.swiper', {
 const showMore = document.querySelector('.brands__more');
 const brandsSlider = document.querySelector('.swiper');
 const imgRotate = document.querySelector('.brands__img');
+
 const moreEquipment = document.querySelector('.equipment__more');
 const equipmentSlider = document.querySelector('.equipment__slider');
 const equipmentRotate = document.querySelector('.equipment__img');
+
+const moreText = document.querySelector('.basic-information__more');
+const text = document.querySelector('.basic-information__container');
+const textImg = document.querySelector('.basic-information__expand');
+
 
 const menuBtnOpen = document.querySelector('.menu__btn');
 const menu = document.querySelector('.menu');
 const menuSmoke = document.querySelector('.menu-smoke');
 const menuBtnClose = document.querySelector('.menu__btn-back');
+
+const modalFeedbackOpen = document.querySelector('.contact__btn-message');
+const modalFeedback = document.querySelector('.modal-feedback');
+const modalSmoke = document.querySelector('.modal-feedback-smoke');
+const modalFeedbackClose = document.querySelector('.modal-feedback__btn-back');
+
+const modalCallOpen = document.querySelector('.contact__btn-phone');
+const modalCall = document.querySelector('.modal-call');
+const modalCallClose = document.querySelector('.modal-call__btn-back');
 
 showMore.addEventListener('click', () => {
     const isBrandsToggled = brandsSlider.classList.toggle('brands__visible');
@@ -49,6 +64,13 @@ moreEquipment.addEventListener('click', () => {
     equipmentRotate.classList.toggle('brands__img-rotate');
 });
 
+moreText.addEventListener('click', () => {
+    const isTextToggled = text.classList.toggle('text-visible');
+    moreText.textContent = isTextToggled ? 'Скрыть' : 'Читать далее';
+    textImg.classList.toggle('text-img--rotate');
+});
+
+
 menuBtnOpen.addEventListener('click', () => {
     menu.classList.toggle('menu__container-open');
     menuSmoke.classList.toggle('menu-smoke--open');
@@ -57,4 +79,24 @@ menuBtnOpen.addEventListener('click', () => {
 menuBtnClose.addEventListener('click', () => {
     menu.classList.remove('menu__container-open');
     menuSmoke.classList.remove('menu-smoke--open');
+});
+
+modalFeedbackOpen.addEventListener('click', () => {
+    modalFeedback.classList.toggle('modal-feedback-open');
+    modalSmoke.classList.toggle('modal-feedback-smoke--open');
+});
+
+modalFeedbackClose.addEventListener('click', () => {
+    modalFeedback.classList.remove('modal-feedback-open');
+    modalSmoke.classList.remove('modal-feedback-smoke--open');
+});
+
+modalCallOpen.addEventListener('click', () => {
+    modalCall.classList.toggle('modal-call-open');
+    modalSmoke.classList.toggle('modal-feedback-smoke--open');
+});
+
+modalCallClose.addEventListener('click', () => {
+    modalCall.classList.remove('modal-call-open');
+    modalSmoke.classList.remove('modal-feedback-smoke--open');
 });
